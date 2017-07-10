6e7be1ce746c1d4541c45370d754314315ee2ac7
aimof <aimof.aimof@gmail.com>
2017-07-11 00:55:29 +0900

    complete loop

--- escape/src/main.rs
+++ escape/src/main.rs
@@ -1,29 +1,38 @@
 extern crate getch;
 
+use getch::Getch;
 use std::sync::mpsc::channel;
 use std::thread::spawn;
+use std::result::Result;
+use std::io::Error;
 
 fn main() {
     println!("If you want to stop, please press esc key!");
     let (sender, receiver) = channel();
-    let mut i = 0;
+
     spawn(move || {
-        loop {
-            println!("{}", i);
-            i = i + 1;
-            if receiver.recv().unwrap() == true {
+        loop{
+            let g: Result<Getch, Error> = Getch::new();
+            let key: Result<u8, Error> = Getch::getch(&g.ok().unwrap());
+            const KEY_X: u8 = 120;
+
+            let input_key = key.ok().unwrap();
+            if input_key == KEY_X {
+                sender.send(true).unwrap();
                 break;
-            } else {
-                continue;
             }
         }
     });
+    let mut i = 0;
     loop {
-        let key = getch::Getch();
-
-        if ch == 0x1b {
-            sender.send(true).unwrap();
+        println!("{}", i);
+        i = i + 1;
+        if receiver.try_recv().ok() != None {
             break;
+        } else {
+            continue;
         }
     }
+
+
 }
