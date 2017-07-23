e98a6ca38c1349a08e8fb29e7a8b229da2e5bac8
aimof <aimof.aimof@gmail.com>
2017-07-23 20:12:12 +0900

    lifetime

--- sort_string/src/main.rs
+++ sort_string/src/main.rs
@@ -2,11 +2,12 @@ use std::io::BufRead;
 
 fn main() {
     loop {
-        let s = read_string();
+        //let s = read_string();
+        let s = "example".to_stirng;
         let exit = "exit".to_string();
         match s {
             exit => break,
-            _ => sort_String(s)
+            _ => println!("{}", sort_string(s))
         }
     }
 }
@@ -17,6 +18,11 @@ fn read_string() -> String {
     return line;
 }
 
-fn sort_string(s: String) -> () {
-
+fn sort_string(s: String) -> (&[i32]) {
+    let mut s_iter = s.into_iter();
+    let mut i32_slice: &[i32];
+    while let Some(c) = s_iter.next() {
+        i32_slice = c.parse().unwrap();
+    }
+    return i32_slice
 }
